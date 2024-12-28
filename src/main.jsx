5682560e955d30a7666f8b26602ebc0f13c86658
andreanvaldy <andrefiliate@gmail.com>
2024-12-28 11:19:40 +0700

    djdj

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import { Provider } from 'react-redux';
 import store from './store'; // Import store Redux
 import './index.css';
 import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
