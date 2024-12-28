ea3fd9fb6571c14f3d17ed03fd8dfc7557480b20
andreanvaldy <andrefiliate@gmail.com>
2024-12-28 11:37:33 +0700

    hhh

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import { Provider } from 'react-redux';
 import store from './store'; // Import store Redux
 import './index.css';
 import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';  // Import BrowserRouter
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}> {/* Membungkus aplikasi dengan Provider */}
-      <App />
+      <BrowserRouter basename="/tugasredux"> {/* Membungkus aplikasi dengan BrowserRouter */}
+        <App />
+      </BrowserRouter>
     </Provider>
   </StrictMode>
 );
