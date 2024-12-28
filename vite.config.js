6970a60e2f13ea6fd52b2eabebe61b6e37d0e654
andreanvaldy <andrefiliate@gmail.com>
2024-12-28 18:37:36 +0700

    jhjj

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Root path untuk GitHub Pages repository
+  base: '/',  // Harus root (/) karena ini adalah repository username.github.io
 });
